a8a6449f4aa2c959e5b8455b3ba03fa4d85c40ba
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2025-05-06 18:43:35 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,14 +1,9 @@
-import {createRoot} from "react-dom/client";
+import {createRoot} from "react-dom/client"
 import {HeaderApp} from "./TravelJournal/header.jsx";
- import {App, Challenge, NinjaTurtle} from './index.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
     <>
         <HeaderApp/>
-        {/*<App/>*/}
-        {/*<Challenge/>*/}
-        {/*<Jokes/>*/}
-        {/*{<NinjaTurtle/>}*/}
     </>
 )
